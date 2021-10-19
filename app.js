d2321a4fe2fdac7ebea28a91ba7b2ba94ae92e67
Sakura87B <jimebohm@gmail.com>
2021-10-18 21:51:13 -0300

    agregue el codigo al repo en github

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require ('express');
+const app = express();
+const path = require('path');
+const PUERTO = 2020;
+const publicPath = path.resolve(__dirname,'./public');
+app.use(express.static(publicPath));
+
+app.get('/', (req,res) => {
+    let htmlpath = path.join(__dirname,'./views/home.html')
+    res.sendFile(htmlpath);
+    });
+
+app.get('/register', (req,res) => {
+let htmlpath = path.join(__dirname,'./views/register.html')
+res.sendFile(htmlpath);
+});
+
+app.get('/login', (req,res) => {
+let htmlpath = path.join(__dirname,'./views/login.html')
+res.sendFile(htmlpath);
+});
+    
+app.listen(2020, () => {
+    console.log('Servidor corriendo en puerto 2020')
+});
+
